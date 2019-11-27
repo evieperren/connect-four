64d2a6aafc37f26cc1af1e8e1fa58332fdec3e66
Evie Perren <evie.perren@simplyhealth.co.uk>
2019-11-27 23:02:58 +0000

    correcting order

--- src/connect-4.js
+++ src/connect-4.js
@@ -1,6 +1,5 @@
 const playGame = document.getElementById('button');
 const gameSection = document.querySelector('.game');
-
 const game = {
     isDisplayed: false,
     No_counters: 0,
@@ -15,7 +14,41 @@ const columns = {
     col6: document.getElementsByClassName('column:6'),
     col7: document.getElementsByClassName('column:7')
 }
+playGame.addEventListener('click', showGame);
 
+function showGame(e) {
+    e.preventDefault();
+    game.isDisplayed = true;
+    createTable()
+
+    if (!game.isDisplayed) {
+        playGame.setAttribute('class', 'game__button--hide')
+        createButton()
+    }
+}
+function createTable() {
+    if (game.isDisplayed) {
+        createDropCounters()
+
+        const board = document.createElement('div');
+        board.setAttribute('class', 'game__board');
+        gameSection.appendChild(board);
+
+        for (i = 1; i <= 7; i++) {
+            const row = document.createElement('div');
+            row.setAttribute('class', `game__board__rows row:${i}`);
+            board.appendChild(row);
+
+            for (k = 0; k <= 7; k++) {
+                const spaces = document.createElement('div');
+                spaces.setAttribute('class', `game__board__spaces column:${k}`);
+
+                row.appendChild(spaces);
+            }
+        }
+        game.isDisplayed = false;
+    }
+}
 function createDropCounters (){
     const dropCounters = document.createElement('div');
     dropCounters.setAttribute('class', 'drop-counters');
@@ -29,12 +62,15 @@ function createDropCounters (){
     
         if(game.No_counters !== 1){
             dropCountersSpaces.addEventListener('click', function(e) {
-                e.target.setAttribute('class', 'drop-counters--selected')
                 game.No_counters += 1;
-
+                
+                for(c = 7; c > i; c--){
+                    console.log(c);
+                }
                 switch(e.target.id){
                     case 'counter--0':
-                        columns.col0[6].setAttribute('class', 'dropped-counter')
+                        columns.col0[6].setAttribute('class', 'dropped-counter');
+                        
                         break;
                     case 'counter--1':
                         columns.col1[6].setAttribute('class', 'dropped-counter')
@@ -64,50 +100,18 @@ function createDropCounters (){
         }
     }
 }
-
-function createTable() {
-    if (game.isDisplayed) {
-        createDropCounters()
-
-        const board = document.createElement('div');
-        board.setAttribute('class', 'game__board');
-        gameSection.appendChild(board);
-
-        for (i = 1; i <= 7; i++) {
-            const row = document.createElement('div');
-            row.setAttribute('class', `game__board__rows row:${i}`);
-            board.appendChild(row);
-
-            for(k = 0; k<=7; k++){
-                const spaces = document.createElement('div');
-                spaces.setAttribute('class', `game__board__spaces column:${k}`);
-
-                row.appendChild(spaces);
-            }
-        }
-        game.isDisplayed = false;
-    }
-}
-function restartButton(){
+function createButton(){
     const restart = document.createElement('button');
     restart.setAttribute('class', 'game__button');
     restart.innerHTML = 'Restart Game';
     gameSection.appendChild(restart);
-    // clear game
+
+    restart.addEventListener('click', clearBoard)
 }
 
-function showGame(e) {
-    e.preventDefault();
-    game.isDisplayed = true;
-    createTable()
-
-    if(!game.isDisplayed){
-        playGame.setAttribute('class', 'game__button--hide')
-        restartButton()
-    }
+function clearBoard(){
+    console.log('clear game');
 }
-
-playGame.addEventListener('click', showGame);
 
 
 
